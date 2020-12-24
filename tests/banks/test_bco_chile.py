5db27d5954026f7559d023418a5f278cb9caa4af
Rodrigo Mendez <rodmendezp@gmail.com>
2020-12-24 15:33:57 -0300

    added tests

---
+++ tests/banks/test_bco_chile.py
@@ -0,0 +1,65 @@
+import os
+import re
+from unittest import TestCase
+from bankdata.banks.bco_chile import MAIL_REGEX
+from bankdata.core.constants import TransactionType as TransType
+
+
+class TestRegex(TestCase):
+    resource_folder = '../../resources/bco_chile/'
+
+    def test_check_expense_regex(self):
+        file_path = os.path.join(self.resource_folder, 'check_expense.html')
+        with open(file_path, encoding='utf-8') as f:
+            text = f.read()
+        reg = MAIL_REGEX[TransType.CHECK_EXPENSE]
+        result = re.search(reg, text, re.DOTALL)
+        self.assertEqual(result.group(1), '1.450')
+
+    def test_check_transfer(self):
+        file_path = os.path.join(self.resource_folder, 'check_transfer.html')
+        with open(file_path, encoding='utf-8') as f:
+            text = f.read()
+        reg = MAIL_REGEX[TransType.CHECK_TRANSFER]
+        result = re.search(reg, text, re.DOTALL)
+        self.assertEqual(result.group(1), '20.450')
+
+    def test_check_withdraw(self):
+        file_path = os.path.join(self.resource_folder, 'check_withdraw.html')
+        with open(file_path, encoding='utf-8') as f:
+            text = f.read()
+        reg = MAIL_REGEX[TransType.CHECK_WITHDRAW]
+        result = re.search(reg, text, re.DOTALL)
+        self.assertEqual(result.group(1), '30.000')
+
+    def test_int_cred_exp(self):
+        file_path = os.path.join(self.resource_folder, 'int_cred_exp.html')
+        with open(file_path, encoding='utf-8') as f:
+            text = f.read()
+        reg = MAIL_REGEX[TransType.INT_CRED_EXPENSE]
+        result = re.search(reg, text, re.DOTALL)
+        self.assertEqual(result.group(1), '18,87')
+
+    def test_int_cred_pay(self):
+        file_path = os.path.join(self.resource_folder, 'int_cred_pay.html')
+        with open(file_path, encoding='utf-8') as f:
+            text = f.read()
+        reg = MAIL_REGEX[TransType.INT_CRED_PAY]
+        result = re.search(reg, text, re.DOTALL)
+        self.assertEqual(result.group(1), '18,87')
+
+    def test_nat_cred_exp(self):
+        file_path = os.path.join(self.resource_folder, 'nat_cred_exp.html')
+        with open(file_path, encoding='utf-8') as f:
+            text = f.read()
+        reg = MAIL_REGEX[TransType.NAT_CRED_EXPENSE]
+        result = re.search(reg, text, re.DOTALL)
+        self.assertEqual(result.group(1), '11.000')
+
+    def test_nat_cred_pay(self):
+        file_path = os.path.join(self.resource_folder, 'nat_cred_pay.html')
+        with open(file_path, encoding='utf-8') as f:
+            text = f.read()
+        reg = MAIL_REGEX[TransType.NAT_CRED_PAY]
+        result = re.search(reg, text, re.DOTALL)
+        self.assertEqual(result.group(1), '66.784')
