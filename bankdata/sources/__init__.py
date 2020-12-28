887c6df2892dbce15018cc18d4356bb06569b620
Rodrigo Mendez <rodmendezp@gmail.com>
2020-12-27 22:20:24 -0300

    added gmail attachment source

--- bankdata/sources/__init__.py
+++ bankdata/sources/__init__.py
@@ -1,4 +1,5 @@
 from bankdata.sources.gmail import GMailBankApi
 from bankdata.sources.gmail_exp import GMailExportBankApi
+from bankdata.sources.gmail_att import GMailAttachmentBankApi
 
-__all__ = ['GMailBankApi', 'GMailExportBankApi']
+__all__ = ['GMailBankApi', 'GMailExportBankApi', 'GMailAttachmentBankApi']
