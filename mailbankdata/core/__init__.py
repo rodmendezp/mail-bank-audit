a7f083ca04e797cbb9b8f3bb8f06e94b380717c0
Rodrigo Mendez <rodmendezp@gmail.com>
2021-01-23 22:02:55 -0300

    Added datetime to transaction

--- mailbankdata/core/__init__.py
+++ mailbankdata/core/__init__.py
@@ -1,26 +1,32 @@
 from typing import Match
-from mailbankdata.core.constants import TransactionType
+from datetime import datetime
+from mailbankdata.core.constants import TransactionType as TransType
 
 
 class Transaction:
-    def __init__(self, ttype: TransactionType, amount=None, int_amount=None, rate=None) -> None:
+    def __init__(self, ttype: TransType, mail_dtime: datetime, amount=None, int_amount=None, rate=None) -> None:
         self.amount = amount
         self.int_amount = int_amount
         self.rate = rate
         self.ttype = ttype
+        self.dtime = None
+        self.mail_dtime = mail_dtime
 
     def __repr__(self) -> str:
         if self.rate:
-            return '(%s, %s, USD %s (%s))' % (self.ttype, self.amount, self.int_amount, self.rate)
+            return '(%s, %s, %s, USD %s (%s))' % (self.mail_dtime, self.ttype, self.amount, self.int_amount, self.rate)
         if self.int_amount:
-            return '(%s, USD %s)' % (self.ttype.name, self.int_amount)
-        return '(%s, %s)' % (self.ttype.name, self.amount)
+            return '(%s, %s, USD %s)' % (self.mail_dtime, self.ttype.name, self.int_amount)
+        return '(%s, %s, %s)' % (self.mail_dtime, self.ttype.name, self.amount)
 
     @classmethod
-    def from_match(cls, match: Match, ttype: TransactionType) -> 'Transaction':
+    def from_match(cls, match: Match, mail_dtime: datetime, ttype: TransType) -> 'Transaction':
         groups_dict = match.groupdict()
         transaction = cls.__new__(cls)
-        kwargs = {'ttype': ttype}
+        kwargs = {
+            'ttype': ttype,
+            'mail_dtime': mail_dtime,
+        }
         if 'nat' in groups_dict:
             kwargs['amount'] = float(groups_dict['nat'].replace('.', '').replace(',', '.'))
         if 'int' in groups_dict:
