08d3465f77105a8ed68a109b1872f5cc2bd6c1a3
Rodrigo Mendez <rodmendezp@gmail.com>
2021-02-07 00:09:31 -0300

    add constructor tests

--- tests/sources/test_gmail_att.py
+++ tests/sources/test_gmail_att.py
@@ -1,10 +1,17 @@
 from datetime import date
 from unittest import TestCase
-from mailbankdata.banks import bco_chile
+from mailbankdata.banks import bco_chile, Bank
 from mailbankdata.sources import GMailAttachmentBankApi
 
 
 class TestGMailAttachmentBankApi(TestCase):
+    def test_constructor(self):
+        GMailAttachmentBankApi(None, Bank.BCO_CHILE, 123456)
+
+    def test_constructor_exc(self):
+        with self.assertRaises(Exception):
+            GMailAttachmentBankApi(None, Bank.BCO_CHILE)
+
     def test_generate_filters(self):
         st_date, end_date = date(1, 1, 1), date(1, 1, 2)
         filters = GMailAttachmentBankApi.generate_filters(bco_chile.EMAIL, st_date, end_date)
