b0ad6c8cb5749badbc709cf470bdcf518d7cf6f7
Rodrigo Mendez <rodmendezp@gmail.com>
2021-02-06 23:41:45 -0300

    initial tet for gmail attachment api

---
+++ tests/sources/test_gmail_att.py
@@ -0,0 +1,13 @@
+from datetime import date
+from unittest import TestCase
+from mailbankdata.banks import bco_chile
+from mailbankdata.sources.gmail_att import GMailAttachmentBankApi
+
+
+class TestGMailAttachmentBankApi(TestCase):
+    def test_generate_filters(self):
+        st_date, end_date = date(1, 1, 1), date(1, 1, 2)
+        filters = GMailAttachmentBankApi.generate_filters(bco_chile.EMAIL, st_date, end_date)
+        expected = {'from': bco_chile.EMAIL, 'after': st_date, 'before': end_date}
+        self.assertEqual(filters, expected)
+    pass
